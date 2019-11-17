c64655c23e4e734a80c0067192fdaf50dab39fea
bczhc <bczhc0@126.com>
2019-11-17 13:10:53 +0800

    2

--- sources/kwm.c
+++ sources/kwm.c
@@ -6,6 +6,38 @@
 
 #define MAX_FIND_KEY_TIME 468
 
+void PrintArr(const char arr[], int len) {
+    int l_ = len - 1;
+    printf("[\n");
+    for (int i = 0; i < l_; ++i) {
+        if (i == 16) printf("\n");
+        printf("%i%c", (int) arr[i], 44);
+    }
+    printf("\n%i]___%u", (int) arr[l_ - 1], (l_ + 1));
+}
+
+long long getFileSize(FILE *fp) {
+    long long sz;
+    fseek(fp, 0L, SEEK_END);
+    sz = (long long) ftell(fp);
+    if (sz == -1) {
+        sz = ftell(fp);
+        printf("Get file size error.\n");
+    }
+    fseek(fp, 0L, SEEK_SET);
+    return sz;
+}
+
+int cmpCharArray(const char *a1, const int a1Len, const char *a2, const int a2Len) {
+    if (a1Len != a2Len) return 0;
+    else {
+        for (int i = 0; i < a1Len; ++i) {
+            if (a1[i] != a2[i]) return 0;
+        }
+    }
+    return 1;
+}
+
 int main(const int argc, char **argv) {
     if (argc != 3) {
         printf("Command [filePath] [destFilePath]\n");
--- sources/kwm.h
+++ sources/kwm.h
@@ -14,44 +14,8 @@
 #endif //C99_KWM_H
 //#include "../zhc.h"
 
+void PrintArr(const char arr[], int len);
 
-void PrintArr(const char arr[], int len) {
-    int l_ = len - 1;
-    printf("[\n");
-    for (int i = 0; i < l_; ++i) {
-        if (i == 16) printf("\n");
-        printf("%i%c", (int) arr[i], 44);
-    }
-    printf("\n%i]___%u", (int) arr[l_ - 1], (l_ + 1));
-}
+long long getFileSize(FILE *fp);
 
-long long getFileSize(FILE *fp) {
-    long long sz;
-    fseek(fp, 0L, SEEK_END);
-    sz = (long long) ftell(fp);
-    if (sz == -1) {
-        sz = _ftelli64(fp);
-        printf("Get file size error.\n");
-    }
-    fseek(fp, 0L, SEEK_SET);
-    return sz;
-}
-
-/*int cmpIntArray(const int *a1, const int a1Len, const int *a2, const int a2Len) {
-    if (a1Len != a2Len) return 0;
-    else {
-        for (int i = 0; i < a1Len; ++i) {
-            if (a1[i] != a2[i]) return 0;
-        }
-    }
-    return 1;
-}*/
-int cmpCharArray(const char *a1, const int a1Len, const char *a2, const int a2Len) {
-    if (a1Len != a2Len) return 0;
-    else {
-        for (int i = 0; i < a1Len; ++i) {
-            if (a1[i] != a2[i]) return 0;
-        }
-    }
-    return 1;
-}
+int cmpCharArray(const char *a1, const int a1Len, const char *a2, const int a2Len);
